ef01cb1ac41d1fd925d840d7ef659794dbe44edb
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-11 18:55:05 +0100

    added extension method with path parameter

---
+++ source/DotNetBay.Health.Owin/HealthFeature.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace DotNetBay.Health.Owin
+{
+    public static class HealthFeature
+    {
+        public static void UseHealth(this IAppBuilder appBuilder, string path)
+        {
+            appBuilder.Map(path, ab => ab.Use<HealthMiddleware>());
+        }
+    }
+}
--- source/DotNetBay.SelfHost/Startup.cs
+++ source/DotNetBay.SelfHost/Startup.cs
@@ -14,7 +14,7 @@ namespace DotNetBay.SelfHost
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
 
-            app.Use<HealthMiddleware>();
+            app.UseHealth("/health");
         }
     }
 }
