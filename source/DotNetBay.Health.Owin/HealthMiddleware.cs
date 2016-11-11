45b4508cc1a22559c895e651de8b02c3cf4e98c4
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-11 18:12:49 +0100

    added empty middleware

---
+++ source/DotNetBay.Health.Owin/HealthMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace DotNetBay.Health.Owin
+{
+    public class HealthMiddleware : OwinMiddleware
+    {
+        public HealthMiddleware(OwinMiddleware next) : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            await this.Next.Invoke(context);
+        }
+    }
+}
