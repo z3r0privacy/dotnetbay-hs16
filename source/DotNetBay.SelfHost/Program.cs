1b3b9a757b5d5f8fbf6bc7a309666e73069fce2e
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-11 18:02:58 +0100

    added project for SelfHosting Owin Pipeline

---
+++ source/DotNetBay.SelfHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace DotNetBay.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var binding = "http://localhost:1901/";
+
+            WebApp.Start<Startup>(binding);
+
+            Console.WriteLine($"Webserver is running on {binding}");
+            Console.WriteLine("");
+
+            Console.Write("Press Enter to quit.");
+            Console.ReadLine();
+        }
+    }
+}
