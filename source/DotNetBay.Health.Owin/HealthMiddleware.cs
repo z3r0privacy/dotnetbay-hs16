a2549166f4cd0452f6f9aa73d650693c87e82054
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-11 18:45:43 +0100

    added middleware logic

--- source/DotNetBay.Health.Owin/HealthMiddleware.cs
+++ source/DotNetBay.Health.Owin/HealthMiddleware.cs
@@ -1,16 +1,32 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DotNetBay.Data.EF;
 using Microsoft.Owin;
 
 namespace DotNetBay.Health.Owin
 {
     public class HealthMiddleware : OwinMiddleware
     {
+        private EFMainRepository repository;
+
         public HealthMiddleware(OwinMiddleware next) : base(next)
         {
+            this.repository = new EFMainRepository();
+
         }
 
         public override async Task Invoke(IOwinContext context)
         {
+            await context.Response.WriteAsync("<h1>Health Information</h1>");
+
+            await context.Response.WriteAsync("<h2>Database Connection String</h2>");
+            await context.Response.WriteAsync(this.repository.Database.Connection.ConnectionString);
+
+            await context.Response.WriteAsync("<h2>Data Health</h2>");
+            await context.Response.WriteAsync("Number of Auctions: " + this.repository.GetAuctions().Count());
+
+            await context.Response.WriteAsync("");
+
             await this.Next.Invoke(context);
         }
     }
