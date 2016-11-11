5d59c51b567908b0f87a417b24586a89f3cd5f3e
Michael Schnyder <michael.schnyder@zuehlke.com>
2016-11-11 18:48:05 +0100

    added missing config & assemplies

--- source/DotNetBay.SelfHost/Program.cs
+++ source/DotNetBay.SelfHost/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Data.Entity.SqlServer;
 using Microsoft.Owin.Hosting;
 
 namespace DotNetBay.SelfHost
 {
     class Program
     {
+        // ROLA - This is a hack to ensure that Entity Framework SQL Provider is copied across to the output folder.
+        // As it is installed in the GAC, Copy Local does not work. It is required for probing.
+        // Fixed "Provider not loaded" error
+        SqlProviderServices ensureDLLIsCopied = SqlProviderServices.Instance;
+
         static void Main(string[] args)
         {
             var binding = "http://localhost:1901/";
